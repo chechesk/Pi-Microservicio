b2d4f478d462400dd7a0c1395d6a6c795412ab82
Leticia <jgromerom89@gmail.com>
2023-10-02 21:55:40 -0300

    Update app.js

--- Characters/app.js
+++ Characters/app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 //Documentacion
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require("./src/doc/swaggerConfig"); // Importa la configuración de Swagger
+
 
 const app = express();
 
@@ -10,11 +9,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 // Agrega la ruta para la documentación de Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/api/docs.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
-  });
+
 
 app.use(require("./src/routes/index"));
 
